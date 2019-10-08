51e283db20c8df1c66cc611322adc6567a0ed541
corebootgit <coreboot@tlen.pl>
2019-10-08 23:35:24 +0200

    Initial commit

---
+++ app/src/main/java/com/example/mysql/MainActivity.java
@@ -0,0 +1,92 @@
+package com.example.mysql;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+//import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.Statement;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String url = "jdbc:mysql://192.168.0.22:3306/test";
+    private static final String user = "marcin";
+    private static final String pass = "zy8ex13";
+    Button btnFetch,btnClear;
+    TextView txtData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txtData = (TextView) this.findViewById(R.id.txtData);
+        btnFetch = (Button) findViewById(R.id.btnFetch);
+        btnClear = (Button) findViewById(R.id.btnClear);
+        btnFetch.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+// TODO Auto-generated method stub
+                ConnectMySql connectMySql = new ConnectMySql();
+                connectMySql.execute("");
+            }
+        });
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txtData.setText("");
+            }
+        });
+
+    }
+
+    private class ConnectMySql extends AsyncTask<String, Void, String> {
+        String res = "";
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            Toast.makeText(MainActivity.this, "Please wait...", Toast.LENGTH_SHORT)
+                    .show();
+
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+                Connection con = DriverManager.getConnection(url, user, pass);
+                System.out.println("Databaseection success");
+
+                String result = "Database Connection Successful\n";
+                Statement st = con.createStatement();
+                ResultSet rs = st.executeQuery("select distinct Country from tblCountries");
+                ResultSetMetaData rsmd = rs.getMetaData();
+
+                while (rs.next()) {
+                    result += rs.getString(1).toString() + "\n";
+                }
+                res = result;
+            } catch (Exception e) {
+                e.printStackTrace();
+                res = e.toString();
+            }
+            return res;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            txtData.setText(result);
+        }
+    }
+
+}
